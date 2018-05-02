2b0f05ceee2c4962bad907090e991f807335b168
Stephen Pennell <stepenn7@gmail.com>
2018-05-02 19:15:10 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# vscode-preferences
+
+## Installation:
+
+```
+git clone git@github.com:steyep/vscode-preferences.git && cd vscode-preferences
+sh install.sh
+```
+
+## Credit:
+Icon and theme from the beautiful [Material-Theme](https://equinsuocha.io/projects/material-theme/).
---
+++ install.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+prefs=
+backup=
+
+vs_code="$HOME/Library/Application Support/Code"
+if [ ! -d "$vs_code" ]; then
+  echo "Could not locate VSCode directory at:"
+  echo "\"$vs_code\""
+  exit 1
+fi
+
+pushd $(dirname "$0") > /dev/null
+  prefs="$PWD"
+  backup="${prefs}/backup"
+  user="${vs_code}/User"
+popd > /dev/null
+
+for dir in "$user" "$backup"; do
+  mkdir -p "$dir"
+done
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+
+  path=$(echo "$line" | sed 's/^Files \(.*\) and.*$/\1/;s/^Only in \(.*\): \(.*\)$/\1\/\2/')
+  echo "$path"
+  # Ignore paths containing or starting with a dot (.):
+  ignore='\.git'
+  [[ $path =~ ${ignore// /|} \
+  || $(basename "$path") =~ ^\.\
+  || -z $path ]] && continue
+
+  # Back up the unique file
+  cp -r "$path" "$backup/${path#*$user/}"
+
+done <<< "$(diff -qr "$user" "$prefs" | grep "$user")"
+
+# Trash original '/User' file
+rm -rf "$user"
+
+# Create the symlink
+ln -s "$prefs/" "$user"
+
+# Update the icon
+find "/Applications/Visual Studio Code.app/Contents/Resources" \
+  -maxdepth 1 \
+  -type f \
+  -name "Code.icns" \
+  -exec sh -c '
+  app="${0%%.app*}.app"
+  cp "$1/icon/icon.icns" "$0"
+  cp -r "$app" "$app.copy"
+  rm -rf "$app"
+  mv "$app.copy" "$app"
+  echo "Icon updated for $app"
+' "{}" "$prefs" \;
+
+
+
